bd806865017f34e2123441ac0d9ac00a6bdfcb66
Nickel 13 <nikesushko@gmail.com>
2018-07-02 23:26:22 +0300

    Working version of schulte table

--- src/js/shulte.js
+++ src/js/shulte.js
@@ -18,63 +18,26 @@ function shuffleNums(arr) {
   return arr;
 }
 
-function addNewNums(arr) {
-  shuffleNums(arr);
-  for (var i = 0; i < arr.length; i++) {
-    board.find(cell).remove();
-  }
-}
-
-console.log(numbers);
-
-var x = numbers[5];
-
-numbers[5] = numbers[6];
-
-numbers[6] = x;
-
-console.log(numbers);
-console.log(shuffleNums(numbers));
-
-
-console.log(cell);
-
 function addNumbers(arr) {
+
+  var newCell = document.createElement('div');
+  newCell.className = 'shulte-table__cell';
+
   for (var i = 0; i < numbers.length; i ++) {
     board.append(cell.clone().html(arr[i]));
   }
   cell[0].remove();
 }
 
-
 function randomNum(max) {
   return Math.round(max - ((max * Math.random())));
 }
 
-var randomnums = [];
-
-for (var i = 1; i <= 25; i++) {
-  randomnums.push(randomNum(25));
+function refreshNumbers() {
+  location.reload();
 }
 
-function getRandomNumbers() {
-  var nums = [];
+console.log(numbers);
+console.log(shuffleNums(numbers));
 
-  for (var i = 0; i < 25; i++) {
-    var newNum = randomNum(25);
-    nums.push(newNum);
-  }
-
-  console.log(nums);
-}
-
-
-// console.log(randomnums);
-// console.log(randomNum());
-// console.log(randomNum());
-// console.log(randomNum());
-// console.log(randomNum());
-console.log(getRandomNumbers());
-console.log("xxxxxx");
-console.log(randomNum(35));
-console.log(cell[0]);
+addNumbers(shuffleNums(numbers));
