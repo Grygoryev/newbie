e7acb743c0b9001e45ac57040ba34800a55dac4e
Nickel 13 <nikesushko@gmail.com>
2018-07-04 21:36:53 +0300

    adding waxom-landing (beginning)

--- dist/js/waxom-landing.js
+++ dist/js/waxom-landing.js
@@ -2,10 +2,8 @@ var projectCard = $('.project-card');
 
 projectCard.mouseenter(function () {
   $(this).toggleClass('project-card_hover');
-  // $(this).toggleClass('project-card__img-hover');
 });
 
 projectCard.mouseleave(function () {
   $(this).toggleClass('project-card_hover');
-  // $(this).toggleClass('project-card__img-hover');
 });
