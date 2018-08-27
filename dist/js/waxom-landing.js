bfde334052004065aabfd1c5252440406191eca0
Nickel 13 <nikesushko@gmail.com>
2018-08-27 10:39:11 +0300

    removing dist

--- dist/js/waxom-landing.js
+++
@@ -1,9 +0,0 @@
-var projectCard = $('.project-card');
-
-projectCard.mouseenter(function () {
-  $(this).toggleClass('project-card_hover');
-});
-
-projectCard.mouseleave(function () {
-  $(this).toggleClass('project-card_hover');
-});
